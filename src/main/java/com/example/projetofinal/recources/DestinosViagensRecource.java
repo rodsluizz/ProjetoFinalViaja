d727eea15b55bce4ccb41cc1850945c43a3c4a50
rodsluizz <rodrigoluizcferreira@gmail.com>
2022-03-16 12:14:45 -0300

    atualização API 16/03

--- src/main/java/com/example/projetofinal/recources/DestinosViagensRecource.java
+++ src/main/java/com/example/projetofinal/recources/DestinosViagensRecource.java
@@ -3,6 +3,7 @@ package com.example.projetofinal.recources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.example.projetofinal.services.DestinosViagensService;
 
 @RestController
 @RequestMapping("/destinosviagens")
+@CrossOrigin(origins = "*")
 public class DestinosViagensRecource {
 	
 	@Autowired
