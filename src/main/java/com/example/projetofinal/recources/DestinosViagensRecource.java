2ef153b113718ff322e00829de98fe0674604a07
rodsluizz <rodrigoluizcferreira@gmail.com>
2022-03-18 19:37:42 -0300

    Atualização 18/03 pt2

--- src/main/java/com/example/projetofinal/recources/DestinosViagensRecource.java
+++ src/main/java/com/example/projetofinal/recources/DestinosViagensRecource.java
@@ -29,7 +29,7 @@ public class DestinosViagensRecource {
 		return destinosViagensService.findAll();
 	}
 	
-	@PostMapping("/cadastro")
+	@PostMapping
 	public DestinosViagens save(@RequestBody DestinosViagens obj) {
 		return destinosViagensService.save(obj);
 	}
